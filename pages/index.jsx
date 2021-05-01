0bb1a2924d94b5f8f043077c1b11ed527b682ee0
Matheus Graciano <matheus.andrade@zarpsystem.com.br>
2021-05-01 15:17:48 -0300

    Remove old login button

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,11 +1,9 @@
 import React from 'react';
-import CreateOrLoginButton from '../src/components/organisms/CreateOrLoginButton';
 import GoogleLoginButton from '../src/components/organisms/GoogleLoginButton';
 
 const Home = () => (
   <section>
     <h1>Hello World</h1>
-    <CreateOrLoginButton />
     <br />
     <GoogleLoginButton />
   </section>

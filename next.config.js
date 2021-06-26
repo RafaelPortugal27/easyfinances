769806f2c6fac1d201314268d0a91bb2d2352760
rafaelportugal27 <rafael.mportugal@gmail.com>
2021-06-26 14:47:25 -0300

    add envirement vairables database

--- next.config.js
+++ next.config.js
@@ -1,7 +1,15 @@
 require('dotenv').config();
 
 module.exports = {
-  publicRuntineConfig: {
+  publicRuntimeConfig: {
     GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
+  },
+  serverRuntimeConfig: {
+    DB_CONNECTION: process.env.DB_CONECTION,
+    DB_HOST: process.env.DB_HOST,
+    DB_DATABASE: process.env.DB_DATABASE,
+    DB_USERNAME: process.env.DB_USERNAME,
+    DB_PASSWORD: process.env.DB_PASSWORD,
+    DB_PORT: process.env.DB_PORT,
   },
 };

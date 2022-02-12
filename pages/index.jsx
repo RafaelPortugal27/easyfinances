d1b48901214472a9299f7f4fe63a411c3601fb2d
RafaelPortugal27 <rafael.mportugal@gmail.com>
2022-02-12 11:51:17 -0300

    undo

--- pages/index.jsx
+++ pages/index.jsx
@@ -4,7 +4,7 @@ import GoogleLogoutButton from '../src/components/organisms/GoogleLogoutButton';
 
 const Home = () => (
   <section>
-    <h1>Hello World a small-change</h1>
+    <h1>Hello World</h1>
     <br />
     <GoogleLoginButton />
     <br />
